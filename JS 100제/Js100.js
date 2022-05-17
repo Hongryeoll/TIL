d06a64a8abb061488bcc7a20aeee2c2eff188e17
Hongryeoll <hong462804@gmail.com>
2022-05-17 23:27:24 +0900

    TIL

--- JS 100제/Js100.js
+++ JS 100제/Js100.js
@@ -79,7 +79,7 @@ console.log(a+b);
 자바스크립트 객체를 다음과 같이 만들었다. 
 출력값을 입력하시오. (출력값은 공백을 넣지 않습니다. )
 
-```jsx
+```
 var d = {
     'height':180,
     'weight':78,
@@ -92,3 +92,27 @@ console.log(d['weight']);
 ```
 
 정답 : 84 (d의 weight의 값이 출력된다.)
+
+// 9번
+
+다음 소스 코드를 완성하여 날짜와 시간을 출력하시오.
+
+```
+var year = '2019';
+var month = '04';
+var day = '26';
+var hour = '11';
+var minute = '34';
+var second = '27';
+
+var result = //빈칸을 채워주세요
+
+console.log(result);
+
+
+출력
+2019/04/26 11:34:27
+```
+
+정답 : var result = year.concat('/',month,'/',day,' ',hour,':',minute,':',second);
+// concat 사용하기 : 매개변수로 전달된 문자열을 메서드를 호출한 문자열에 붙여 새로운 문자열로 반환한다.
---
+++ 혼자공부하는자바스크립트/220517.js
@@ -0,0 +1,29 @@
+// a부터 b까지 더하는 함수
+// 1부터 100까지
+// 1부터 500까지
+
+function sumAll(a,b) {
+    let outpput = 0
+    for (let i = a; i <= b; i++) {
+        output += i
+        
+    }
+    return output
+}
+console.log(`1부터 100까지의 합: ${sumAll(1,100)}`);
+console.log(`1부터 500까지의 합: ${sumAll(1,500)}`);
+
+// a부터 b까지 곱하는 함수
+// 1부터 10까지
+// 1부터 50까지
+
+function 곱하기All(a,b) {
+    let result = 1
+    for (let i = a; i <= b; i++) {
+        result *= i
+        
+    }
+    return result
+}
+console.log(`1부터 10까지의 곱: ${곱하기All(1,10)}`);
+console.log(`1부터 50까지의 곱: ${곱하기All(1,50)}`);
