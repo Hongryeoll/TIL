dcd58413be1a5db0f7873b7782dc856bc5cab2b6
Hongryeoll <hong462804@gmail.com>
2022-06-25 19:23:00 +0900

    28번 문제 풀이

---
+++ JS 100제/Js100_28.js
@@ -0,0 +1,24 @@
+2-gram이란 문자열에서 2개의 연속된 요소를 출력하는 방법입니다. 
+
+예를 들어 'Javascript'를 2-gram으로 반복해 본다면 다음과 같은 결과가 나옵니다.
+
+
+Javascript
+
+출력
+J a
+a v
+v a
+a s
+s c
+c r
+r i
+i p
+p t
+
+//정답
+const data = prompt('문자를 입력하세요.');
+
+for (let i=0; i<data.length-1; i++){
+  console.log(data[i], data[i+1]);
+}
